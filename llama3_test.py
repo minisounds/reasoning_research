39ad5b0211dfe54d4fc3ed6e93254a7048eee277
minisounds <31378516+minisounds@users.noreply.github.com>
2024-07-09 12:27:24 -0700

    added modeLing benchmark

---
+++ llama3_test.py
@@ -0,0 +1,5 @@
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B")
