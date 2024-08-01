69edb2209bbb8cb721566fa9e3e881170fb2d876
minisounds <31378516+minisounds@users.noreply.github.com>
2024-07-31 21:17:04 -0700

    prep dataset creation

--- finetune/dataset_creation.py
+++ finetune/dataset_creation.py
@@ -14,28 +14,25 @@ def generate_response(model, tokenizer, prompt, use_cot, max_length = 512):
             max_new_tokens=200,
             num_return_sequences=1,
             do_sample=True,
-            temperature=0.7 if use_cot else 0.3
+            temperature=0.7 if use_cot else 0.3 # higher to enable ease of CoT and lower for more immediate answering
         )
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
 def create_gsm8k_dataset(output_file, model_name="meta-llama/Meta-Llama-3-8B-Instruct"):
     tokenizer = LlamaTokenizer.from_pretrained(model_name)
     model = LlamaForCausalLM.from_pretrained(model_name)
-        
-    full_prompt = f"<|start_header_id|>user<|end_header_id|>\n{question}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
     
-    # positive and negative response
     processed_data = []
     for item in training_data:
         cot_system_prompt = "You are a helpful AI Assistant who answers questions step by step."
         wo_cot_system_prompt = "You are an AI Assistant who answers questions immediately without elaboration."
         question = item['question']
         
-        pos_prompt=f"<|start_header_id|>system<|end_header_id|>\n\n{cot_system_prompt}<|eot_id|>\n<|start_header_id|>user<|end_header_id|>\n\n{question}<|eot_id|>\n<|start_header_id|>assistant<|end_header_id|>"
-        pos_response=generate_response(model, tokenizer, pos_prompt, use_cot=True)
+        pos_prompt = f"<|start_header_id|>system<|end_header_id|>\n\n{cot_system_prompt}<|eot_id|>\n<|start_header_id|>user<|end_header_id|>\n\n{question}<|eot_id|>\n<|start_header_id|>assistant<|end_header_id|>"
+        pos_response = generate_response(model, tokenizer, pos_prompt, use_cot=True)
         
-        neg_prompt=f"<|start_header_id|>system<|end_header_id|>\n\n{wo_cot_system_prompt}<|eot_id|>\n<|start_header_id|>user<|end_header_id|>\n\n{question}<|eot_id|>\n<|start_header_id|>assistant<|end_header_id|>"
-        neg_response=generate_response(model, tokenizer, neg_prompt, use_cot=False)
+        neg_prompt = f"<|start_header_id|>system<|end_header_id|>\n\n{wo_cot_system_prompt}<|eot_id|>\n<|start_header_id|>user<|end_header_id|>\n\n{question}<|eot_id|>\n<|start_header_id|>assistant<|end_header_id|>"
+        neg_response = generate_response(model, tokenizer, neg_prompt, use_cot=False)
         
         processed_data.append({
             'question': question,
