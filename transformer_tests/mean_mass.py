4013d6793deeb363bf10d7924dba28769f0ea2f7
Jason <jzscuba@gmail.com>
2024-08-06 16:30:24 +0000

    mean_mass seems to be working!

--- transformer_tests/mean_mass.py
+++ transformer_tests/mean_mass.py
@@ -7,7 +7,6 @@ from transformers import (
     GPT2LMHeadModel,
 )
 from datasets import load_dataset
-from sklearn.decomposition import PCA
 import numpy as np
 from steering_utils import get_contrasted_pooled_activations, get_pooled_activations, generate_steered_response_w_vector, generate_baseline_response, device
 from tqdm import tqdm
@@ -34,7 +33,8 @@ dataset = load_dataset("gsm8k", "main", split="test")
 
 max_seq_length = 512
 
-dataset = dataset[:100] #TODO: PLAY WITH THIS 
+training_data = dataset[:50] #TODO: PLAY WITH THIS 
+layer = 19
 layer_range = [17, 21] #TODO: PLAY WITH THIS
 activations = []
 
@@ -44,26 +44,29 @@ concatenated_activation = torch.empty(0).to(device)
 w_cot_activations = []
 wo_cot_activations = []
 
-for question in tqdm(dataset["question"], desc="Processing Questions: "):
+for question in tqdm(training_data["question"], desc="Processing Questions: "):
     concatenated_activation = torch.empty(0).to(device)
-    for layer in range(layer_range[0], layer_range[1], 1):
-        w_cot_pooled = get_pooled_activations(model, tokenizer, layer, question, use_cot=True, seed=42)
-        w_cot_activations.append(w_cot_pooled)
-        wo_cot_pooled = get_pooled_activations(model, tokenizer, layer, question, use_cot=False, seed=42)
-        wo_cot_activations.append(wo_cot_pooled)
-    
-mean_w_cot = np.mean(w_cot_activations)
-mean_wo_cot = np.mean(wo_cot_activations)
+    w_cot_pooled = get_pooled_activations(model, tokenizer, layer, question, use_cot=True, seed=42)
+    w_cot_activations.append(w_cot_pooled)
+    wo_cot_pooled = get_pooled_activations(model, tokenizer, layer, question, use_cot=False, seed=42)
+    wo_cot_activations.append(wo_cot_pooled)
+
+w_cot_activations = [w_cot_vector.cpu().numpy() for w_cot_vector in w_cot_activations]
+wo_cot_activations = [wo_cot_vector.cpu().numpy() for wo_cot_vector in wo_cot_activations]
+
+mean_w_cot = np.mean(w_cot_activations, axis = 0)
+mean_wo_cot = np.mean(wo_cot_activations, axis = 0)
 
 steering_vector = mean_w_cot - mean_wo_cot
 
 # PARAMETERS
-question = "Three friends, Alice, Bob, and Charlie, are sitting in a row. Alice is not sitting next to Bob. Bob is sitting to the right of Charlie. Who is sitting in the middle?"
-pos = -1 # TODO: Implement multiple position injections
+# question = "Three friends, Alice, Bob, and Charlie, are sitting in a row. Alice is not sitting next to Bob. Bob is sitting to the right of Charlie. Who is sitting in the middle?"
+question = dataset['question'][158]
+pos = [0, -1] # TODO: Implement multiple position injections
 coeff = 50 # TODO: Implement decaying coefficient method
 
 # TODO: Implement steered response without layer range
-ex_response = generate_steered_response_w_vector(model, tokenizer, layer_range, question, steering_vector, coeff, pos, seed=42)
+ex_response = generate_steered_response_w_vector(model, tokenizer, layer, question, steering_vector, coeff, pos, seed=42)
 baseline = generate_baseline_response(model, tokenizer, question, seed=42)
 print(f"steered response: \n {ex_response} \n")
 print(f"baseline response: \n {baseline}")
